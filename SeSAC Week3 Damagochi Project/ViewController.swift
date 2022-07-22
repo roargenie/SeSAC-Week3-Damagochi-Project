7cd39ed3e3e6a206fee9cd75ac9965bd626e0050
roargenie <105975078+roargenie@users.noreply.github.com>
2022-07-22 15:36:51 +0900

    Initial Commit

---
+++ SeSAC Week3 Damagochi Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSAC Week3 Damagochi Project
+//
+//  Created by 이명진 on 2022/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
