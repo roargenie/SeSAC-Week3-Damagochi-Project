b190ab0e349aa7002f3525d70eeff79ad3a7a33f
roargenie <105975078+roargenie@users.noreply.github.com>
2022-07-23 17:59:53 +0900

    ✅메인화면 구성(로직이 미완성임)

--- SeSAC Week3 Damagochi Project/DamagochiModel.swift
+++ SeSAC Week3 Damagochi Project/DamagochiModel.swift
@@ -9,23 +9,23 @@ struct Damagochi {
     }
     
     var image: UIImage?
-    var name: String?
-    var introduce: String?
-    static var description: [String] = []
-    static var rice: Double = 0
-    static var water: Double = 0
-    static var level: Double = 1
-        
+    var name: String
+    var introduce: String
+    var description: [String] = []
+    static var rice: Int = 0
+    static var water: Int = 0
+    static var level: Int = 1
+    
     static var exp: Double {
         get {
-            let result = ((rice / 5) + (water / 2)) / 10
-            //level += result
-            return result.rounded(.down)
+            let rice = Double(rice) / Double(5)
+            let water = Double(water) / Double(2)
+            
+            let result = rice + water
+            return result
             
         }
-        set {
-            
-        }
+        
     }
    
 }
@@ -34,30 +34,11 @@ struct Damagochi {
 struct DamagochiInfo {
     
     var damagochi: [Damagochi] = [
-        Damagochi(image: UIImage(named: "\(Damagochi.Characters.RawValue.self)-\(Damagochi.level)"), name: "따끔따끔 다마고치", introduce:
-                    """
-                 저는 선인장 다마고치 입니당.
-                 키는 2cm, 몸무게는  150g이에요.
-                 성격은 소심하지만 마음은 따뜻해요.
-                 열심히 잘 먹고 잘 클 자신은 있답니당!
-                 반가워요 대장님!
-                 """),
-        Damagochi(image: UIImage(named: "\(Damagochi.Characters.RawValue.self)-\(Damagochi.level)"), name: "방실방실 다마고치", introduce:
-                  """
-                  저는 방실방실 다마고치 입니당.
-                  키는 100km 몸무게는 150톤이에용!
-                  성격은 화끈하고 따뜻하답니당~!
-                  열심히 잘 먹고 잘 클 자신은 있답니당!
-                  반가워요 대장님!
-                  """),
-        Damagochi(image: UIImage(named: "\(Damagochi.Characters.RawValue.self)-\(Damagochi.level)"), name: "반짝반짝 다마고치", introduce:
-                    """
-                    저는 반짝반짝 다마고치 입니당.
-                    키는 50km 몸무게는 100톤이에용!
-                    성격은 따뜻하지만 화나면 무서워용!
-                    열심히 잘 먹고 잘 클 자신은 있답니당!
-                    반가워요 대장님!
-                    """)
+        
+        Damagochi(image: UIImage(named: "1-\(Damagochi.level)"), name: "따끔따끔 다마고치", introduce: "저는 선인장 다마고치 입니당.\n키는 2cm, 몸무게는 150g이에요.\n성격은 소심하지만 마음은 따뜻해요.\n열심히 잘 먹고 잘 클 자신은 있답니당!\n반가워요 대장님!", description: []),
+        Damagochi(image: UIImage(named: "\(Damagochi.Characters.RawValue.self)-\(Damagochi.level)"), name: "방실방실 다마고치", introduce: "저는 방실방실 다마고치 입니당.\n키는 100km 몸무게는 150톤이에용!\n성격은 화끈하고 따뜻하답니당~!\n열심히 잘 먹고 잘 클 자신은 있답니당!\n반가워요 대장님!", description: []),
+        Damagochi(image: UIImage(named: "\(Damagochi.Characters.RawValue.self)-\(Damagochi.level)"), name: "반짝반짝 다마고치", introduce: "저는 반짝반짝 다마고치 입니당.\n키는 50km 몸무게는 100톤이에용!\n성격은 따뜻하지만 화나면 무서워용!\n열심히 잘 먹고 잘 클 자신은 있답니당!\n반가워요 대장님!", description: [])
+        
     ]
 }
 
--- SeSAC Week3 Damagochi Project/MainViewController.swift
+++ SeSAC Week3 Damagochi Project/MainViewController.swift
@@ -1,6 +1,7 @@
 
 
 import UIKit
+import Toast
 
 class MainViewController: UIViewController {
     
@@ -9,10 +10,10 @@ class MainViewController: UIViewController {
     var damagochiData = DamagochiInfo()
     
     var level = Damagochi.level
+    var exp = Damagochi.exp
     var rice = Damagochi.rice
     var water = Damagochi.water
     
-    var descriptionArr: [Double] = [1, 0, 0]
     
     @IBOutlet weak var textImage: UIImageView!
     
@@ -33,78 +34,98 @@ class MainViewController: UIViewController {
     @IBOutlet weak var waterButton: UIButton!
     
     
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        riceTextField.keyboardType = .numberPad
-        waterTextField.keyboardType = .numberPad
+        riceTextField.delegate = self
+        waterTextField.delegate = self
         
+
+        configureUI()
+           
+    }
+    
+    func configureUI() {
         
-        
-        
-        
-        
-        
+        textLabel.text = damagochiData.damagochi[0].introduce
+        damagochiImage.image = damagochiData.damagochi[0].image
+        damagochiNameLabel.text = damagochiData.damagochi[0].name
+        descriptionLabel.text = "LV\(level) • 밥알\(rice)개 • 물방울\(water)"
         
     }
     
-    func designLabel() {
-        descriptionLabel.text = "Lv\(Int(level)) • 밥알\(Int(rice))개 • 물방울\(Int(water))개"
-        
-    }
     
     @IBAction func buttonTapped(_ sender: UIButton) {
-        
-        if sender.tag == 0 {
+        let result = ((Double(rice) / 5.0 + Double(water) / 2.0).rounded(.down)) / 10
+        level = Int(result)
+        print(level)
+        switch sender.tag {
             
-            
-            
-            
-            
-            
-        } else if sender.tag == 1 {
-            
-            
-            
-            
+        case 0:
+            if riceTextField.text == nil {
+                rice += 1
+            }
+            if let riceCount = Int(riceTextField.text!) {
+                if riceCount < 100 {
+                    rice += riceCount
+                    
+                    if level <= 10 && level >= 1 {
+                        descriptionLabel.text = "LV\(Int(result)) • 밥알\(rice)개 • 물방울\(water)개"
+                        
+                        damagochiImage.image = UIImage(named: "1-\(level)")
+                    } else if result > 10 {
+                        view.makeToast("더 이상 자라면 안돼요!", duration: 1, position: .center)
+                    } else {
+                        descriptionLabel.text = "LV1 • 밥알\(rice)개 • 물방울\(water)개"
+                    }
+                } else {
+                    //descriptionLabel.text = "LV\(Int(result)) • 밥알\(rice)개 • 물방울\(water)개"
+                    view.makeToast("이렇게 많이 먹을수 없어요!", duration: 1, position: .center)
+                }
+            }
+        case 1:
+//            if waterTextField.text == nil {
+//                water += 1
+//            }
+            if let waterCount = Int(waterTextField.text!) {
+                if waterCount < 50 {
+                    water += waterCount
+                    
+                    if level <= 10 && level >= 1 {
+                        descriptionLabel.text = "LV\(Int(result)) • 밥알\(rice)개 • 물방울\(water)개"
+                        
+                        damagochiImage.image = UIImage(named: "1-\(level)")
+                    } else if result > 10 {
+                        view.makeToast("더 이상 자라면 안돼요!", duration: 1, position: .center)
+                    } else {
+                        descriptionLabel.text = "LV1 • 밥알\(rice)개 • 물방울\(water)개"
+                    }
+                } else {
+                    //descriptionLabel.text = "LV\(Int(result)) • 밥알\(rice)개 • 물방울\(water)개"
+                    view.makeToast("이렇게 많이 먹을수 없어요!", duration: 1, position: .center)
+                }
+            }
+        default:
+            break
         }
-        
-        
-        
-        
-        
-    }
-    
-    
-    @IBAction func riceButtonTapped(_ sender: UIButton) {
-        
-        
-        guard let riceCount = riceTextField.text, Int(riceCount) != nil, !riceCount.isEmpty else { return }
-        rice += Double(riceCount)!
-        
-        descriptionLabel.text = "Lv\(Damagochi.exp) • 밥알\(Int(rice))개 • 물방울\(Int(water))개"
-        
-        
-    }
-    
-    @IBAction func waterButtonTapped(_ sender: UIButton) {
-        guard let waterCount = waterTextField.text, Int(waterCount) != nil else { return }
-        
-        
-        
-        
-        
-    }
-    
-    @IBAction func tapGestureTapped(_ sender: UITapGestureRecognizer) {
-        view.endEditing(true)
-    }
-    
-    
-    
-    
-    
 
+    }
+
+}
+
+extension MainViewController: UITextFieldDelegate {
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        print(string)
+        
+        if Int(string) != nil || string == "" {
+            return true
+        }
+        return false
+    }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        riceTextField.resignFirstResponder()
+        waterTextField.resignFirstResponder()
+    }
     
 }
